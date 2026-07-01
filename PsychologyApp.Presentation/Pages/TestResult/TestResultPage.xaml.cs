74418e93b931c43d2b8c50660c0f5fa0617d63e9
masuk <masuk@LENOVOMAXIM>
2026-07-01 19:14:12 +1000

    -fixes

---
+++ PsychologyApp.Presentation/Pages/TestResult/TestResultPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using PsychologyApp.Presentation.App.Providers;
+using PsychologyApp.Presentation.Entities.Test;
+
+namespace PsychologyApp.Presentation.Pages.TestResult;
+
+public partial class TestResultPage : ContentPage
+{
+    public TestResultPage(ITestResultViewModelFactory factory, TestResultInfo result)
+    {
+        BindingContext = factory.Create(this, result);
+        InitializeComponent();
+    }
+}
